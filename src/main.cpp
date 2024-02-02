bc04ac9bae3679189a65ed66895c33cad29c5b75
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-02-02 15:08:19 -0500

    add unclickable option (steam only)

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "Geode/platform/cplatform.h"
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CCSprite.hpp>
 
@@ -6,9 +7,15 @@ using namespace geode::prelude;
 class $modify(CCSprite) {
 	static CCSprite* createWithSpriteFrameName(char const* frameName) {
         CCSprite* sprite = CCSprite::createWithSpriteFrameName(frameName);
-
-        if (strcmp(frameName, "GJ_pauseBtn_clean_001.png") == 0 && Mod::get()->getSettingValue<bool>("enabled"))
-			sprite->setVisible(false); // hide pause button
+        
+        if (!Mod::get()->getSettingValue<bool>("enabled")) return sprite;
+        if (strcmp(frameName, "GJ_pauseBtn_clean_001.png") == 0) {
+            sprite->setVisible(false); // hide pause button
+            #ifdef GEODE_IS_DESKTOP
+            if (Mod::get()->getSettingValue<bool>("unclickable"))
+                sprite->setScale(0.0); // make pause button unclickable
+            #endif
+        }
         return sprite;
     }
 };
