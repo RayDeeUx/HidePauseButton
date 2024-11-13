c130afc6692582cd2d8e161587543048581e0a15
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-11-13 12:35:59 -0500

    what the hell

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,19 @@
-#include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 
 using namespace geode::prelude;
 
 class $modify(MyPlayLayer, PlayLayer) {
-	static void onModify(auto & self)
-    {
-        self.setHookPriority("PlayLayer::init", 1000);
-    }
-	bool init(GJGameLevel* p0, bool p1, bool p2) {
-		if (!PlayLayer::init(p0, p1, p2)) return false;
-		if (!Mod::get()->getSettingValue<bool>("enabled")) return true;
+	void setupHasCompleted() {
+		PlayLayer::setupHasCompleted();
+		if (!Mod::get()->getSettingValue<bool>("enabled")) return;
 		auto uiLayer = getChildByID("UILayer");
-        auto pauseButton = uiLayer->getChildByIDRecursive("pause-button");
-		if (pauseButton == nullptr) return true;
-        if (!Mod::get()->getSettingValue<bool>("unclickable")) {
-            auto pauseButtonSprite = pauseButton->getChildren()->objectAtIndex(0);
-            typeinfo_cast<CCSprite*>(pauseButtonSprite)->setScale(0);
-        } else {
-            pauseButton->setScale(0);
-        }
-		return true;
+		if (!uiLayer) return;
+		auto pauseButton = uiLayer->getChildByIDRecursive("pause-button");
+		if (!pauseButton) return;
+		if (!Mod::get()->getSettingValue<bool>("unclickable")) {
+			static_cast<CCSprite*>(pauseButton->getChildren()->objectAtIndex(0))->setScale(0);
+		} else {
+			pauseButton->setScale(0);
+		}
 	}
 };
